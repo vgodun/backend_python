5e306cd340585814bd2edf088fec0a8641d2a042
vgodun <vgodunsoftware@gmail.com>
2025-05-22 20:59:26 +0300

    fix: comment

--- project.py
+++ project.py
@@ -1,15 +1,15 @@
-# initial_amount = float(input('the initial amount of money: '))
-# interest_rate = float(input('annual interest rate: '))
-# years=int(input('number of years: '))
-#
-# result= initial_amount * (1+interest_rate / 100)**years
-# print(round(result,2))
+initial_amount = float(input('the initial amount of money: '))
+interest_rate = float(input('annual interest rate: '))
+years=int(input('number of years: '))
 
-# sumRes=float(input('sum of money: '))
-# interest_rate=float(input('annual interest rate: '))
-# percentage=sumRes/(1+interest_rate/100)
-#
-# print(round(percentage,2))
+result= initial_amount * (1+interest_rate / 100)**years
+print(round(result,2))
+
+sumRes=float(input('sum of money: '))
+interest_rate=float(input('annual interest rate: '))
+percentage=sumRes/(1+interest_rate/100)
+
+print(round(percentage,2))
 
 income=float(input('Enter income: '))
 needs_percent = 50
