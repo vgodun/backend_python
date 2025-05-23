ba2f0070b2aa337db21d3c5c6ad9b24dff09951d
vgodun <146578980+vgodun@users.noreply.github.com>
2025-05-23 15:47:16 +0300

    Add feature all task in primitive data types (#5)
    * Add feature all task in primitive data types
    
    * fix: comment
    
    * fix: folder & space
    
    * fix: folder & space
    
    * Delete feature_1.txt
    
    * Delete file.txt
    
    ---------
    
    Co-authored-by: vgodun <vgodunsoftware@gmail.com>

---
+++ module_1_task_5/task_3.py
@@ -0,0 +1,6 @@
+is_logged_in = True
+has_permission = False
+
+print(is_logged_in and has_permission)
+print(is_logged_in or has_permission)
+print(not has_permission)
---
+++ module_1_task_5/task_9.py
@@ -0,0 +1,5 @@
+num1 = 17
+num2 = 4
+
+print('Floor division result:', num1 // num2)
+print('Remainder', num1 % num2)
