5976e6c6e38466fe59c5c117f9c7a68531fc2b11
vgodun <vgodunsoftware@gmail.com>
2025-06-14 20:29:09 +0300

    feature: high function

--- module_1_task_20/project.py
+++ module_1_task_20/project.py
@@ -26,13 +26,13 @@ def main():
     }
 
     print("1: double  2: square  3: add_five  4: negate")
-    selected = input("Введи номери функцій через кому (наприклад: 1,3): ").split(",")
-    value = float(input("Введи число: "))
+    selected = input("Enter the function numbers separated by commas (for example: 1,3): ").split(",")
+    value = float(input("Enter number: "))
 
     chosen_funcs = [funcs[num.strip()] for num in selected if num.strip() in funcs]
     result = compose(*chosen_funcs)(value)
 
-    print("Результат:", result)
+    print("Result:", result)
 
 if __name__ == "__main__":
     main()
